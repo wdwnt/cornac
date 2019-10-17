435e2068abd75e981b83de70a30a0d5a22df2e76
Robbie Page <robbie.page@e-gineering.com>
2019-10-17 19:02:47 -0400

    trying to get Alexa to play audio clips

--- src/util/converters/alexa-converter.ts
+++ src/util/converters/alexa-converter.ts
@@ -1,27 +1,27 @@
 import { DialogFlowResponse } from '../../models/response/dialogflow/dialogflow-response';
 
-export function convertDialogflowResponseToAlexa(response: DialogFlowResponse) {
-    const GOOGLE_ITEMS = response.payload.google.richResponse.items;
-    const simpleResponse = GOOGLE_ITEMS[0].simpleResponse;
-    const lastResponse = GOOGLE_ITEMS[GOOGLE_ITEMS.length - 1].simpleResponse;
+export function convertDialogflowResponseToAlexa(response: DialogFlowResponse, isSSML = true) {
+    const googleItems = response.payload.google.richResponse.items;
+    const simpleResponse = googleItems[0].simpleResponse;
+    const lastResponse = googleItems[googleItems.length - 1].simpleResponse;
 
     const alexaResponse = {
         response: {
             outputSpeech: {
                 ssml: simpleResponse.textToSpeech,
                 text: simpleResponse.displayText,
-                type: 'PlainText'
+                type: isSSML ? 'SSML' : 'PlainText'
             },
             reprompt: {
                 outputSpeech: {
                     ssml: lastResponse.textToSpeech,
                     text: lastResponse.displayText,
-                    type: 'PlainText'
+                    type: isSSML ? 'SSML' : 'PlainText'
                 }
             },
             shouldEndSession: response.payload.google.expectUserResponse
         },
-        version: 'v1'
+        version: '1.0'
     };
 
     return alexaResponse;
